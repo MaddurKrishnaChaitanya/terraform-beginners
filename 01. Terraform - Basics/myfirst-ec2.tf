68bd4a01cadb7c3f6f0fe428858c3d0e1b857393
kittu <maddur.krishnachaitanya@gmail.com>
2020-08-19 12:35:30 +0530

    basic ec2 instance creation

--- 01. Terraform - Basics/myfirst-ec2.tf
+++ 01. Terraform - Basics/myfirst-ec2.tf
@@ -5,7 +5,7 @@ provider "aws" {
   secret_key = "/fWUoFZ7eDyn3uipVLJ078Md0NX3Wey9Kuoty92F"
 }
 
-# resource::: EC2 instance
+# resource::: EC2 instance with jenkins server
 resource "aws_instance" "jenkinsServer" {
   ami           = "ami-0ebc1ac48dfd14136"
   instance_type = "t2.micro"
