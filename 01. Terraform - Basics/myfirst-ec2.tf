b617ccab649210b21c9a027ab73a129225e369e9
kittu <maddur.krishnachaitanya@gmail.com>
2020-08-19 14:27:36 +0530

    code update

--- 01. Terraform - Basics/myfirst-ec2.tf
+++ 01. Terraform - Basics/myfirst-ec2.tf
@@ -1,14 +1,14 @@
 # provider info with authentication
 provider "aws" {
   region     = "ap-south-1"
-  access_key = "AKIARH265V76FFFBJJZW"
-  secret_key = "/fWUoFZ7eDyn3uipVLJ078Md0NX3Wey9Kuoty92F"
+  access_key = "your access key"
+  secret_key = "your secret_key"
 }
 
 # resource::: EC2 instance with jenkins server
 resource "aws_instance" "jenkinsServer" {
-  ami           = "ami-0ebc1ac48dfd14136"
-  instance_type = "t2.micro"
+  ami           = "ami-0ebc1ac48dfd14136" // AMI id inside above mentioned region
+  instance_type = "t2.micro" // instance type
   tags = {
     Name = "Jenkins_Server"
   }
